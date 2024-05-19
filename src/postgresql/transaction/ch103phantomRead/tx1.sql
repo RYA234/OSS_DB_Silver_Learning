f7841cc7ba39c22c3d4260fbae369ec91b26c629
RYA234 <mryuabc@gmail.com>
2024-05-20 06:20:55 +0900

    コメント追加

--- src/postgresql/transaction/ch103phantomRead/tx1.sql
+++ src/postgresql/transaction/ch103phantomRead/tx1.sql
@@ -4,15 +4,16 @@ CREATE TABLE tbl(id int,value int);
 GRANT ALL ON tbl TO tx2;
 INSERT INTO tbl VALUES(1,5),(2,8);
 SELECT * FROM tbl;
+
 BEGIN;
 SET TRANSACTION ISOLATION LEVEL  READ COMMITTED ;
 SHOW transaction_isolation;
-SELECT pg_sleep(4);
+
+SELECT pg_sleep(4);   -- tx2.sql の最初のSELECTを待つ
 UPDATE tbl SET value=10 WHERE id=1;
-SELECT pg_sleep(4);
+SELECT pg_sleep(4);   -- tx2.sql の2回目のSELECTを待つ
 INSERT INTO tbl VALUES(3,12);
 COMMIT;
+
 SELECT * FROM tbl;
 SELECT pg_sleep(4);
--- SELECT * FROM tbl ORDER BY id;
--- SELECT pg_sleep(4);
--- src/postgresql/transaction/ch103phantomRead/tx2.sql
+++ src/postgresql/transaction/ch103phantomRead/tx2.sql
@@ -1,13 +1,13 @@
 -- docker-compose  exec -it postgresql  psql -U tx2 -f src/postgresql/transaction/ch103phantomRead/tx2.sql -d docker
 
-SELECT pg_sleep(2);
+SELECT pg_sleep(2); -- tx1.sql のトランザクション開始を待つ
 BEGIN;
 SET TRANSACTION ISOLATION LEVEL  READ COMMITTED;
 
 SHOW transaction_isolation;
-SELECT pg_sleep(4);
+SELECT pg_sleep(4); -- tx1.sql の更新を待つ
 SELECT * FROM tbl;
-SELECT pg_sleep(4);
-SELECT * FROM tbl;
+SELECT pg_sleep(4); -- tx1.sql の挿入を待つ
+SELECT * FROM tbl;  -- ここでファントムリードが発生
 
 COMMIT;
